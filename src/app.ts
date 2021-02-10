ae848028ce725140eabf599290cab29cba2d00b0
Gerard Absi <gerard.absi90@gmail.com>
2021-02-10 08:07:09 -0800

    chore removed public

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import bodyParser from 'body-parser';
 import compression from 'compression';
-import path from 'path';
 import express, { Request, Response, NextFunction } from 'express';
 import ApplicationError from './errors/application-error';
 import routes from './routes';
@@ -12,8 +11,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3001);
-
-app.use(express.static(path.join(__dirname, 'public'), { maxAge: 31557600000 }));
 
 app.use(routes);
 
